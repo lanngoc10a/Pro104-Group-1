b48f5e3775d7d585aafe2ac069b759182db768b6
Nikolai <cooprix1@gmail.com>
2021-05-19 11:58:42 +0200

    remove age

--- scripts/employee-generator.js
+++ scripts/employee-generator.js
@@ -10,7 +10,7 @@ import EmployeeModule from '../Modules/EmployeeModule.js'
                 <article class="column is-4 has-text-centered">
                     <img src="../Images/employees/${employee.image}" alt="Photo of ${employee.name}">
                     
-                    <h3 class="title is-3">${employee.name}, ${employee.age}</h3>
+                    <h3 class="title is-3">${employee.name}</h3>
                     <p class="subtitle is-5">${employee.position}</p>
                 </article>
             `;
