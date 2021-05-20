a67c44e70e6006122c467a1df4e2447c4fe4c8bb
asvinaa <asvinaa.95@gmail.com>
2021-05-20 08:10:26 +0200

    meny

---
+++ scripts/meny.js
@@ -0,0 +1,16 @@
+var newTittleTxt = document.getElementById("add-tittel-txt");
+var addRecipeTxt =document.getElementById("add-recipe-txt");
+var addnewRecipeBtn= document.getElementById("add-recipe-btn"); 
+var addRecipeUl =document.getElementById("add-recipe-ul");
+
+var newTittle;
+var addRecipe;
+
+function addnewRecipe(){
+    newTittle = newTittleTxt.value;
+    addRecipeUl.innerHTML += `<h1>${newTittle}</h1>`;
+
+    addRecipe = addRecipeTxt.value;
+    addRecipeUl.innerHTML += `<p >  ${addRecipe} . </p>`;
+}
+addnewRecipeBtn.onclick = addnewRecipe
