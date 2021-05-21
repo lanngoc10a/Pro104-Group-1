6558325095ba8f989baa058095d74bffc3f81149
asvinaa <asvinaa.95@gmail.com>
2021-05-21 21:56:16 +0200

    add all

--- scripts/meny.js
+++ scripts/meny.js
@@ -14,3 +14,43 @@ function addnewRecipe(){
     addRecipeUl.innerHTML += `<p >  ${addRecipe} . </p>`;
 }
 addnewRecipeBtn.onclick = addnewRecipe;
+
+//popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+// tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(item => item.classList.remove('is-active'))
+        tab.classList.add('is-active');
+
+        const target = tab.dataset.target;
+        tabContentBoxes.forEach(box => {
+            if (box.getAttribute('id') === target) {
+                box.classList.remove('is-hidden');   
+            } else {
+                box.classList.add('is-hidden');
+            } 
+        });
+    })
+})
+
+
+//modal
+const recipeButton = document.querySelector('.recipe');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('.modal');
+
+recipeButton.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+modalBg.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
